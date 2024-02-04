b6bbbac4be454b0a52f1e86d175a8562b0b14cee
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-02-04 17:03:09 -0300

    vecell insights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'App Cripto Datavisio';
+  title = 'App Criptos Datavisio';
   apptitle= 'Cripto Datavisio';
 }
